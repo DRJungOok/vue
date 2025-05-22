c9e3dc72cf9e4e01e557c90dbd992e37ee67b847
DRJungOok <DRJungOok@gmail.com>
2025-05-22 22:02:17 +0900

    study day 1

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,16 @@ import {ref, reactive} from "vue";
 
 const message = ref('hello world');
 const dynamicId = ref('dynamicId');
-const counter = reactive({
-  count: 0
-})
-
-console.log(counter.count);
-counter.count++;
+const count = ref(0);
+function increment() {
+  count.value++;
+}
 </script>
 
 <template>
   <div>{{message}}</div>
   <div :id="dynamicId">{{dynamicId}}</div>
+  <button @click="increment">count value : {{count}}</button>
 </template>
 
 <style scoped>
