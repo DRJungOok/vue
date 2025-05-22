de567e56b882e02d5d91b26c232a54b4e2c1dfc1
DRJungOok <DRJungOok@gmail.com>
2025-05-22 15:29:40 +0900

    create vue Project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref, reactive} from "vue";
+
+const message = ref('hello world');
+const dynamicId = ref('dynamicId');
+const counter = reactive({
+  count: 0
+})
+
+console.log(counter.count);
+counter.count++;
+</script>
+
+<template>
+  <div>{{message}}</div>
+  <div :id="dynamicId">{{dynamicId}}</div>
+</template>
+
+<style scoped>
+#dynamicId {
+  color: red
+}
+</style>
